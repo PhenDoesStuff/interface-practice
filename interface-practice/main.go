7b5c5fcb906d650cff4606edfd174dab5b231dda
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-06 10:47:45 -0400

    Example of how code would look without interfaces - printGreeting() is same code but has different value types. Interfaces help this problem

--- interface-practice/main.go
+++ interface-practice/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+// For these functions, we can omit the value (eb/sb) because it is not being used
+// EX: func (eb englishBot) --> eb not needed in this case
+func (englishBot) getGreeting() string {
+	// Very custom logic for generatig an English greeting :)
+	return "Hello there!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func (spanishBot) getGreeting() string {
+	// Very custom logic for generating a Spanish greeting :)
+	return "Hola!"
+}
+
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
 }
